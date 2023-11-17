39883831666614d004582f4ce388df640f8b558f
huumai1998 <huumai0918@gmail.com>
2023-11-16 17:48:13 -0700

    add

--- src/components/todoList.tsx
+++ src/components/todoList.tsx
@@ -1,19 +1,28 @@
-const TodoList = () => {
+import { ITask } from "@/types/task";
+import React from "react";
+
+interface TodoListProps {
+  tasks: ITask[];
+}
+
+const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
   return (
     <div className="overflow-x-auto">
       <table className="table">
         {/* head */}
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Favorite Color</th>
+            <th>Tasks</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Cy Ganderton</td>
-            <td>Blue</td>
-          </tr>
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td>{task.id}</td>
+              <td>{task.text}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
